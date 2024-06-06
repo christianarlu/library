11ae57fb3ae03493a24864d8958accdf4c085721
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-06-07 05:48:21 +0800

    Add styles to dialog backdrop

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ newBookButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     modal.close();
 });
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+});
