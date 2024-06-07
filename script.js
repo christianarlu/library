0c754698551665bb97a02814d340d92c84c3dce1
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-06-07 21:28:43 +0800

    Added functionality to add Books and display them on the table

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const myLibrary = [];
+const book1 = new Book("Game of thrones", "George R.R. Martin", 1945, false);
+const book2 = new Book("Harry Potter", "JK Rowling", 1997, true);
+const book3 = new Book("Noli Me Tangere", "Jose P. Rizal", 1878, false);
 
+const myLibrary = [book1, book2];
+
+const table = document.querySelector("table");
+const submit = document.querySelector(".submit");
+
+const modal = document.querySelector("[data-modal]");
+const newBookButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+
+// BOOK CONSTRUCTOR FUNCTION
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -7,23 +19,73 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+// }
+
+
+
+// SUBMIT FUNCTION
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputText = document.querySelectorAll("input:not(input[type=radio], input[type=submit])");
+    let inputRadio = document.querySelector("input[type=radio]:checked");
+    
+    let title = inputText[0].value;
+    let author = inputText[1].value;
+    let pages = inputText[2].value;
+    let read = inputRadio.value;
+
+    const book = new Book(title, author, pages, read);
+    addBook(book)
+    clearInputs();
+    modal.close();
+});
+
+// CLEAR FUNCTION
+function clearInputs() {
+    let inputText = document.querySelectorAll("input:not(input[type=radio], input[type=submit])");
+    let radios = document.querySelectorAll("input[type=radio]");
+
+    inputText[0].value = "";
+    inputText[1].value = "";
+    inputText[2].value = "";
+    for (let i = 0; i < radios.length; i++ ) {
+        radios[i].checked = false;
+    }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+// ADD BOOK FUNCTION
+function addBook(bookObj) {
+    myLibrary.push(bookObj);
+    const newRow = document.createElement("tr");
+    table.appendChild(newRow);
+    for (let key in bookObj) {
+        const newTd = document.createElement("td");
+        newRow.appendChild(newTd);
+        const newText = document.createTextNode(bookObj[key]);
+        newTd.appendChild(newText);
+    }
 }
 
-const book1 = new Book("Game of thrones", "George R.R. Martin", 1945, false);
-const book2 = new Book("Harry Potter", "JK Rowling", 1997, true);
-const book3 = new Book("Noli Me Tangere", "Jose P. Rizal", 1878, false);
+// DISPLAY FUNCTION
+function displayBooks(myLibrary) {
+    myLibrary.forEach((book) => {
+        const newRow = document.createElement("tr");
+        table.appendChild(newRow);
+        for (let key in book) {
+            const newTd = document.createElement("td");
+            newRow.appendChild(newTd)
 
-const modal = document.querySelector("[data-modal]");
-const newBookButton = document.querySelector("[data-open-modal]");
-const closeButton = document.querySelector("[data-close-modal]");
+            const newText = document.createTextNode(book[key]);
+            newTd.appendChild(newText);
+        }
+    });          
+}
 
+displayBooks(myLibrary);
 
+/* Dialog/Modal related stuff below */
 newBookButton.addEventListener("click", () => {
     modal.showModal();
 });
@@ -43,3 +105,4 @@ modal.addEventListener("click", e => {
         modal.close();
     }
 });
+
